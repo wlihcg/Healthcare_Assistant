b49e12639dee01c627a514215f57e8f9fb5e886b
wli <wli@hcg.com>
2024-04-16 12:19:57 -0400

    changed chat title and description

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -653,8 +653,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Healthcare Assistant</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>This assistant is configured to help answer any of your insurance related questions</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
